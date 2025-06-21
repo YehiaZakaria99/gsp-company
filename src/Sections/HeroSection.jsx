df613a1a43705cbefab56c8a283d9e3321f916f1
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-06-21 14:18:02 +0300

    update-v06

--- src/Sections/HeroSection.jsx
+++ src/Sections/HeroSection.jsx
@@ -1,6 +1,7 @@
 import img1 from "/HomePage/HeroSection/1.jpg";
 import img2 from "/HomePage/HeroSection/2.webp";
-import img3 from "/HomePage/HeroSection/3.jpg";
+import img3 from "/HomePage/HeroSection/3.webp";
+import img4 from "/HomePage/HeroSection/4.jpeg";
 
 // Swiper
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -9,20 +10,21 @@ import "swiper/css";
 import "swiper/css/effect-fade";
 import "swiper/css/autoplay";
 
-// Framer Motion
-import { motion } from "framer-motion";
-import { TypeAnimation } from "react-type-animation";
 import { cn } from "../lib/utils";
-import { ArrowDown, ChevronDown } from "lucide-react";
 import { Link } from "react-scroll";
 
-export default function HeroSection() {
-  const slides = [img1, img2, img3];
+// Animation
+import { motion } from "framer-motion";
+import { TypeAnimation } from "react-type-animation";
 
-  function handleScroll() {}
+export default function HeroSection() {
+  const slides = [img1, img2, img3, img4];
 
   return (
-    <section id="heroSection" className="relative h-screen w-full overflow-hidden">
+    <section
+      id="heroSection"
+      className="relative h-screen w-full overflow-hidden"
+    >
       <Swiper
         effect={"fade"}
         autoplay={{ delay: 3500, disableOnInteraction: false }}
@@ -72,13 +74,10 @@ export default function HeroSection() {
             </div>
             <div
               className={cn(
-                "absolute left-1/2 transform -translate-x-1/2 bottom-8 z-40",
+                "absolute left-1/2 transform -translate-x-1/2 bottom-8 z-40"
               )}
             >
-              {/* <span className="text-sm mb-2 ">Scroll</span> */}
               <Link to="aboutSection" smooth={true} duration={0} offset={-50}>
-                {/* <ArrowDown className="h-10 w-10 text-navbar cursor-pointer"></ArrowDown> */}
-                {/* <ChevronDown className="h-10 w-10 text-navbar cursor-pointer" /> */}
                 <div className="w-6 h-10 border-2 border-navbar cursor-pointer rounded-full flex items-start justify-center p-1 mx-auto animate-bounce">
                   <div className="w-1 h-2 bg-navbar rounded-full " />
                 </div>
