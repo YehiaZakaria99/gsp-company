f80921b682eab89452d5368b24edd19c91f32cec
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-07-09 22:22:53 +0300

    Edit scroll up

--- src/Components/Scroll/ScrollUp.jsx
+++ src/Components/Scroll/ScrollUp.jsx
@@ -1,14 +1,20 @@
 import { ArrowUp } from "lucide-react";
 import { Link } from "react-scroll";
+import { cn } from "../../lib/utils";
 
-export default function ScrollUp({to}) {
+export default function ScrollUp({ to }) {
   return (
     <>
       {/* Scroll Up */}
       <div className="fixed right-10 bottom-8 z-[9999999] transition-all duration-300">
         <Link to={to} smooth={true} duration={0} offset={-50}>
-          <div className="border-2 border-primary cursor-pointer rounded-full flex items-start justify-center p-1 mx-auto bg-white transition-all duration-300">
-            <ArrowUp className="text-primary font-extrabold" size={25} />
+          <div
+            className={cn(
+              "border-2 border-primary cursor-pointer rounded-full flex items-start justify-center p-1 mx-auto bg-white text-primary",
+              "transition-all duration-300 hover:bg-primary hover:text-white hover:border-white"
+            )}
+          >
+            <ArrowUp className=" font-extrabold" size={25} />
           </div>
         </Link>
       </div>
