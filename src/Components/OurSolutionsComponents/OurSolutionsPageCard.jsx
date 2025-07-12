b3ffc646107cecf673b03206603acdcb3732edd1
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-07-13 01:38:12 +0300

    Edit Some Styles

--- src/Components/OurSolutionsComponents/OurSolutionsPageCard.jsx
+++ src/Components/OurSolutionsComponents/OurSolutionsPageCard.jsx
@@ -3,12 +3,11 @@ import { cn } from "./../../lib/utils";
 
 export default function OurSolutionsPageCard({
   index,
-  solution: { id, img, title, description },
+  solution: { img, title, description },
 }) {
   return (
     <>
       <div
-        // id={id}
         className={cn(
           "w-full flex flex-col items-center justify-center gap-0 md:gap-10 px-4 max-w-7xl mx-auto",
           index % 2 !== 0 ? "md:flex-row-reverse" : "md:flex-row"
@@ -17,22 +16,24 @@ export default function OurSolutionsPageCard({
         {/* Image */}
         <div className="w-full md:w-1/2  ">
           <div className="my-8">
-            <div className="cursor-pointer flex h-[200px] sm:h-[300px] md:h-[350px] shadow-lg rounded-3xl overflow-hidden shadow-primary bg-primary/20">
+            <div className="relative group cursor-pointer flex h-[200px] sm:h-[300px] md:h-[350px] shadow-lg rounded-3xl overflow-hidden shadow-primary bg-primary/20">
               <img
-                //   data-aos={index % 2 !== 0 ? "flip-right" : "flip-left"}
                 src={img}
                 alt={title}
-                className=" w-full h-full object-cover transition-transform duration-500 hover:scale-125"
+                className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-125"
               />
+              <div
+                className={cn(
+                  "absolute inset-0 z-10 bg-hover/25",
+                  "transition duration-500 group-hover:bg-transparent"
+                )}
+              ></div>
             </div>
           </div>
         </div>
 
         {/* Content */}
-        <div
-          //   data-aos={index % 2 !== 0 ? "fade-right" : "fade-left"}
-          className="w-full md:w-1/2 flex flex-col justify-center text-center md:text-left space-y-4"
-        >
+        <div className="w-full md:w-1/2 flex flex-col justify-center text-center md:text-left space-y-4">
           <h3 className="text-3xl md:text-3xl font-bold text-primary">
             {title}
           </h3>
--- src/Sections/ContactSection.jsx
+++ src/Sections/ContactSection.jsx
@@ -1,118 +1,138 @@
 import { Mail, MapPin, Phone, ArrowRight } from "lucide-react";
 import { Link } from "react-router";
 import { cn } from "./../lib/utils";
+import { Fade } from "react-awesome-reveal";
 
 export default function ContactSection() {
   return (
-    <section className="bg-navBg relative overflow-hidden py-20 px-4 lg:px-32">
-      <div className="absolute -top-10 -left-10 w-64 h-64 bg-mainColor opacity-10 rounded-full blur-3xl z-0"></div>
-      <div className="max-w-4xl mx-auto text-center relative z-10">
-        <h3
+    <section className="relative bg-navBg py-24 px-4 md:px-12 lg:px-24 xl:px-32 overflow-hidden">
+      <div className="absolute -top-20 -left-20 w-80 h-80 bg-mainColor/10 rounded-full blur-3xl z-0"></div>
+      <div className="absolute bottom-0 right-0 w-64 h-64 bg-mainColor/20 rounded-full blur-3xl z-0"></div>
+
+      <div className="relative z-10 max-w-6xl mx-auto text-center">
+        {/* header */}
+        <h2
           className="text-3xl md:text-4xl font-bold text-primary mb-4"
           data-aos="fade-up"
         >
-          Contact Us
-        </h3>
+          Get In Touch
+        </h2>
         <p
-          className="text-primary text-lg md:text-xl mb-12"
+          className="text-lg md:text-xl text-primary/80 mb-12 max-w-2xl mx-auto"
           data-aos="fade-up"
           data-aos-delay="100"
         >
-          Whether you have a question, a business inquiry, or just want to say
-          hello — we're here to listen.
+          Have a question or need assistance? We're just one message away.
         </p>
 
-        {/* Map */}
+        {/* map */}
         <div
-          className="w-full h-[400px] mb-12 rounded-2xl overflow-hidden shadow-xl border border-mainColor/10"
+          className="rounded-3xl overflow-hidden shadow-2xl border border-mainColor/10 mb-16"
           data-aos="zoom-in-up"
         >
           <iframe
-            title="gspLocation"
-            className="w-full h-full"
-            src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d915417.7871715222!2d49.758230847358675!3d26.331332553463806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d30.0192266!2d31.2727774!4m5!1s0x3e49e9b296d1f973%3A0xa03ccdc4c9c1c993!2sAL%20BANDARIYAH%20TOWER%2C%208627%2BWMG%2C%20Al%20Bandariyah%2C%20Al%20Khobar%2034424%2C%20Saudi%20Arabia!3m2!1d26.302312999999998!2d50.214204699999996!5e0!3m2!1sen!2seg!4v1752067534992!5m2!1sen!2seg"
+            title="GSP Location"
+            className="w-full h-[400px]"
             loading="lazy"
+            src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d915417.7871715222!2d49.758230847358675!3d26.331332553463806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d30.0192266!2d31.2727774!4m5!1s0x3e49e9b296d1f973%3A0xa03ccdc4c9c1c993!2sAL%20BANDARIYAH%20TOWER%2C%208627%2BWMG%2C%20Al%20Bandariyah%2C%20Al%20Khobar%2034424%2C%20Saudi%20Arabia!3m2!1d26.302312999999998!2d50.214204699999996!5e0!3m2!1sen!2seg!4v1752067534992!5m2!1sen!2seg"
           ></iframe>
         </div>
 
-        {/* Contact Info Cards */}
-        <div className="grid gap-6 md:grid-cols-3 mb-12">
-          {/* Email */}
-          <div
-            className="bg-white/10 backdrop-blur-md rounded-xl p-6 text-primary flex flex-col items-center gap-3"
-            // data-aos="fade-up"
-            // data-aos-delay="100"
-          >
-            <div className="bg-mainColor text-navBg p-3 rounded-full">
-              <Mail size={24} />
-            </div>
-            <div className="text-center text-sm">
-              <p className="font-semibold">Emails</p>
-              <a
-                href="mailto:info@gsp-sa.com"
-                className="hover:underline block"
+        <div className="grid gap-8 md:grid-cols-3 mb-16">
+          <Fade duration={800} cascade damping={0.2} triggerOnce>
+            {/* Email */}
+            <div
+              className={cn("contact-sec-card", "group")}
+              data-aos="fade-up"
+              data-aos-delay="100"
+            >
+              <div
+                className={cn(
+                  "icon-box",
+                  "group-hover:bg-white group-hover:text-primary"
+                )}
               >
-                info@gsp-sa.com
-              </a>
-              <a
-                href="mailto:mohammed@gsp-sa.com"
-                className="hover:underline block"
-              >
-                mohammed@gsp-sa.com
-              </a>
+                <Mail size={24} />
+              </div>
+              <p className="font-semibold text-base">Emails</p>
+              <div className="text-sm mt-1 space-y-1 text-center">
+                <a
+                  href="mailto:info@gsp-sa.com"
+                  className="hover:underline block"
+                >
+                  info@gsp-sa.com
+                </a>
+                <a
+                  href="mailto:mohammed@gsp-sa.com"
+                  className="hover:underline block"
+                >
+                  mohammed@gsp-sa.com
+                </a>
+              </div>
             </div>
-          </div>
 
-          {/* Phone */}
-          <div
-            className="bg-white/10 backdrop-blur-md rounded-xl p-6 text-primary flex flex-col items-center gap-3"
-            data-aos="fade-up"
-            data-aos-delay="200"
-          >
-            <div className="bg-mainColor text-navBg p-3 rounded-full">
-              <Phone size={24} />
-            </div>
-            <div className="text-center text-sm">
-              <p className="font-semibold">Phone</p>
-              <a href="tel:+966138813042" className="hover:underline">
+            {/* Phone */}
+            <div
+              className={cn("contact-sec-card", "group")}
+              data-aos="fade-up"
+              data-aos-delay="200"
+            >
+              <div
+                className={cn(
+                  "icon-box",
+                  "group-hover:bg-white group-hover:text-primary"
+                )}
+              >
+                <Phone size={24} />
+              </div>
+              <p className="font-semibold text-base">Phone</p>
+              <a
+                href="tel:+966138813042"
+                className="text-sm hover:underline mt-1"
+              >
                 +966 13 881 3042
               </a>
             </div>
-          </div>
 
-          {/* Address */}
-          <div
-            className="bg-white/10 backdrop-blur-md rounded-xl p-6 text-primary flex flex-col items-center gap-3"
-            data-aos="fade-up"
-            data-aos-delay="300"
-          >
-            <div className="bg-mainColor text-navBg p-3 rounded-full">
-              <MapPin size={24} />
-            </div>
-            <div className="text-center text-sm">
-              <p className="font-semibold">Location</p>
-              <span>
+            {/* Address */}
+            <div
+              className={cn("contact-sec-card", "group")}
+              data-aos="fade-up"
+              data-aos-delay="300"
+            >
+              <div
+                className={cn(
+                  "icon-box",
+                  "group-hover:bg-white group-hover:text-primary"
+                )}
+              >
+                <MapPin size={24} />
+              </div>
+              <p className="font-semibold text-base">Location</p>
+              <p className="text-sm mt-1 text-center">
                 Al Bandariyah Business Centre, Office No 105,
                 <br />
                 Al Amir Faisal Bin Fahd Rd, Khobar, K.S.A
-              </span>
+              </p>
             </div>
-          </div>
+          </Fade>
         </div>
 
-        {/* CTA Button */}
-        <div data-aos="fade-up" data-aos-delay="400">
-          <Link
-            to="/contact"
-            className={cn(
-              "inline-flex items-center gap-2 px-6 py-3 border-2 border-primary text-primary rounded-lg group",
-              "hover:bg-primary hover:text-white hover:border-white transition duration-300"
-            )}
-          >
-            Send a Message
-            <ArrowRight className="group-hover:translate-x-1 transition-transform duration-300" />
-          </Link>
-        </div>
+        {/* CTA */}
+        <Fade direction="left" duration={800} triggerOnce>
+          <div>
+            <Link
+              to="/contact"
+              className={cn(
+                "group inline-flex gap-3 items-center text-md px-6 py-2 rounded-md border border-primary text-primary",
+                "hover:bg-primary hover:text-white transition-all duration-300 font-semibold"
+              )}
+            >
+              Send a Message
+              <ArrowRight className="transition-transform duration-300 group-hover:translate-x-1" />
+            </Link>
+          </div>
+        </Fade>
       </div>
     </section>
   );
